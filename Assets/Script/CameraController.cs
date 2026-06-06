410ea84de4841b71fc5e7a1d46f81ef7f0a1e4c2
沟鼠 <Nezumi404NotFound@Gmail.com>
2026-06-06 17:05:32 +0800

    Latest

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera playerCamera;
+    public GameObject cameraFollowPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void LateUpdate()
+    {
+        //相机跟随玩家水平高度
+        playerCamera.transform.position = new Vector3(playerCamera.transform.position.x, cameraFollowPosition.transform.position.y, playerCamera.transform.position.z);
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -18,21 +18,23 @@ public class PlayerController : MonoBehaviour
     private float smoothZ;
     private float currentVelocityX;
     private float currentVelocityZ;
-    private Vector3 currentVelocity;
     public float smoothTime = 0.15f;
 
 
     // 控制变量
+    private enum PlayerMovingState { Walking, Running, Courching}
+    private PlayerMovingState nowState;
     private float moveSpeed = 2f;
-    private bool isRunning = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        nowState = PlayerMovingState.Walking;
     }
 
     void Update()
     {
+        Debug.Log(nowState);
         //设置输入延迟，帮助混合树动画过渡
         smoothX = Mathf.SmoothDamp(smoothX, horizontalInput, ref currentVelocityX, smoothTime);
         smoothZ = Mathf.SmoothDamp(smoothZ, verticalInput, ref currentVelocityZ, smoothTime);
@@ -47,29 +49,35 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        //设置不同移动状态下的速度
         Vector2 input = context.ReadValue<Vector2>();
-        if (isRunning) 
+        if (nowState == PlayerMovingState.Running) 
         {
             horizontalInput = input.x * 1.5f;
             verticalInput = input.y * 1.5f;
         }
-        else if(!isRunning)
+        else if(nowState == PlayerMovingState.Walking)
         {
             horizontalInput = input.x;
             verticalInput = input.y;    
+        }
+        else if (nowState == PlayerMovingState.Courching)
+        {
+            horizontalInput = input.x * 0.7f;
+            verticalInput = input.y * 0.7f;
         }
     }
     public void OnRun(InputAction.CallbackContext context) 
     {
         if (context.performed) 
         {
-            if (isRunning == true) 
+            if (nowState == PlayerMovingState.Running) 
             {
-                isRunning = false;
+                nowState = PlayerMovingState.Walking;
             }
             else
             {
-                isRunning = true;
+                nowState = PlayerMovingState.Running;
             }
         }
     }
@@ -80,26 +88,15 @@ public class PlayerController : MonoBehaviour
             // 如果已经处于下蹲动画
             if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Crouch"))
             {
+                nowState = PlayerMovingState.Walking;
                 playerAnimator.SetBool("IsCourching", false);
+                
             }
             //  不处于下蹲动画
             else 
             {
+                nowState = PlayerMovingState.Courching;
                 playerAnimator.SetBool("IsCourching", true);
-            }
-        }
-    }
-    public void OnSlash(InputAction.CallbackContext context) 
-    {
-        if (context.performed) 
-        {
-            if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Slash"))
-            {
-                return;
-            }
-            else
-            {
-                playerAnimator.SetTrigger("Slash_trigger");
             }
         }
     }
